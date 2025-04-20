243bfd26a747dc85d61d6fd340afd91aa24d09cd
ahmedasaber <ahmeda.saber22@gmail.com>
2025-04-20 19:06:30 +0200

    commit

--- lib/controllers/bloc/counter_state.dart
+++ lib/controllers/bloc/counter_state.dart
@@ -1,13 +1,18 @@
 part of 'counter_bloc.dart';
 
-sealed class CounterState {
+sealed class CounterState extends Equatable{
   final int count;
 
   CounterState(this.count);
+
+  @override
+  List<Object?> get props => [count];
 }
 
 final class CounterInitial extends CounterState {
   CounterInitial() : super(0);
+
+
 }
 
 final class CounterUpdate extends CounterState {
