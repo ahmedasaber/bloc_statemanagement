36f0efaa8b1280d56c2b752ab27470111de044bc
ahmedasaber <ahmeda.saber22@gmail.com>
2025-04-28 02:34:00 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -47,52 +47,80 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(widget.title),
-      ),
-      body: BlocListener<LoginBloc, LoginState>(
-        listener: (context, state) {
-          if (state is LoginSuccess) {
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(SnackBar(content: Text('Login Success')));
-          } else if (state is LoginFailure) {
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(SnackBar(content: Text(state.message)));
-          }
-        },
-        child: BlocBuilder<LoginBloc, LoginState>(
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              TextField(
-                controller: _userController,
-                decoration: InputDecoration(labelText: 'username'),
-              ),
-              TextField(
-                controller: _passController,
-                obscureText: true,
-                decoration: InputDecoration(labelText: 'password'),
-              ),
-              SizedBox(height: 10,),
-              Center(
-                child: state is LoginLoading
-                  ? CircularProgressIndicator()
-                  : ElevatedButton(onPressed: () {
-                      context.read<LoginBloc>().add(Login(username: _userController.text, password: _passController.text));
-                    }, child: Text('login')),
-              ),
-            ],
+      appBar: AppBar(backgroundColor: Colors.white, title: Text(widget.title)),
+      body:
+      // BlocListener<LoginBloc, LoginState>(
+      //   listener: (context, state) {
+      //     if (state is LoginSuccess) {
+      //       ScaffoldMessenger.of(
+      //         context,
+      //       ).showSnackBar(SnackBar(content: Text('Login Success')));
+      //     } else if (state is LoginFailure) {
+      //       ScaffoldMessenger.of(
+      //         context,
+      //       ).showSnackBar(SnackBar(content: Text(state.message)));
+      //     }
+      //   },
+      //   child: BlocBuilder<LoginBloc, LoginState>(
+      //     builder: (context, state) {
+      //       return
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              controller: _userController,
+              decoration: InputDecoration(labelText: 'username'),
+            ),
+            TextField(
+              controller: _passController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'password'),
+            ),
+            SizedBox(height: 10),
+            BlocConsumer<LoginBloc, LoginState>(
+              listener: (context, state) {
+                if (state is LoginSuccess) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Login Success'),
+                      duration: Duration(seconds: 1),
                     ),
-          );
-        },
+                  );
+                } else if (state is LoginFailure) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(state.message),
+                      duration: Duration(seconds: 1),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return Center(
+                  child: state is LoginLoading
+                    ? CircularProgressIndicator()
+                    : ElevatedButton(
+                      onPressed: () {
+                        context.read<LoginBloc>().add(
+                          Login(
+                            username: _userController.text,
+                            password: _passController.text,
+                          ),
+                        );
+                      },
+                      child: Text('login'),
+                    ),
+                );
+              },
+            ),
+          ],
         ),
-      ),
+      ), //;
+      //},
+      //),
+      //),
     );
   }
 }
